79315c65206af3177fb27bf846d37ed5bbb6fe29
leon20121005 <leon20121005@gmail.com>
2019-10-28 04:02:44 +0800

    conduct the second experiment

--- e2.py
+++ e2.py
@@ -12,7 +12,7 @@ import numpy as np
 
 def preprocess_image(image):
     image = image.resize((224, 224))
-    image.show()
+    # image.show()
     image = img_to_array(image)
     image = imagenet_utils.preprocess_input(image)
     image = np.expand_dims(image, axis = 0)
@@ -20,8 +20,8 @@ def preprocess_image(image):
 
 if __name__ == '__main__':
     images = []
-    for index in range(4):
-        image = Image.open(f'{index}.jpg')
+    for index in range(1049, 1125):
+        image = Image.open(f'tvsum_data/tvsum_data/frame{index}.jpg')
         image = preprocess_image(image)
         images.append(image)
 
@@ -40,13 +40,13 @@ if __name__ == '__main__':
         predicted_class_name = labels[predicted_label]
         print('Predicted Class: ', predicted_label, ', Class Name: ', predicted_class_name)
 
-    for index in range(3):
-        print('Cosine similarity 1, 2: ', cosine_similarity(outputs[0][index], outputs[1][index]))
-        print('Cosine similarity 1, 3: ', cosine_similarity(outputs[0][index], outputs[2][index]))
-        print('Cosine similarity 1, 4: ', cosine_similarity(outputs[0][index], outputs[3][index]))
-        print('Cosine similarity 2, 3: ', cosine_similarity(outputs[1][index], outputs[2][index]))
-        print('Cosine similarity 2, 4: ', cosine_similarity(outputs[1][index], outputs[3][index]))
-        print('Cosine similarity 3, 4: ', cosine_similarity(outputs[2][index], outputs[3][index]))
+    # for index in range(3):
+    #     print('Cosine similarity 1, 2: ', cosine_similarity(outputs[0][index], outputs[1][index]))
+    #     print('Cosine similarity 1, 3: ', cosine_similarity(outputs[0][index], outputs[2][index]))
+    #     print('Cosine similarity 1, 4: ', cosine_similarity(outputs[0][index], outputs[3][index]))
+    #     print('Cosine similarity 2, 3: ', cosine_similarity(outputs[1][index], outputs[2][index]))
+    #     print('Cosine similarity 2, 4: ', cosine_similarity(outputs[1][index], outputs[3][index]))
+    #     print('Cosine similarity 3, 4: ', cosine_similarity(outputs[2][index], outputs[3][index]))
 
     for index in range(3):
         embeddings = []
@@ -54,8 +54,13 @@ if __name__ == '__main__':
             embeddings.append(output[index][0])
         pca = PCA(n_components = 2)
         reduced_embeddings = pca.fit_transform(embeddings)
+
+        scenes = [reduced_embeddings]
+        for scene in scenes:
+            print(len(scene))
+
         plt.figure()
-        for jndex in range(4):
-            plt.scatter(reduced_embeddings[jndex][0], reduced_embeddings[jndex][1], label = f'frame {jndex + 1}')
+        for scene_index, scene in enumerate(scenes):
+            plt.scatter([frame[0] for frame in scene], [frame[1] for frame in scene], label = f'scene {scene_index + 1}')
         plt.legend()
     plt.show()
--- e3.py
+++ e3.py
@@ -16,8 +16,8 @@ def get_blue_code(value):
 if __name__ == '__main__':
     images = []
 
-    for index in range(4):
-        image = Image.open(f'{index}.jpg')
+    for index in range(218, 242):
+        image = Image.open(f'tvsum_data/tvsum_data/frame{index}.jpg')
         images.append(image)
 
     histograms = []
@@ -26,30 +26,32 @@ if __name__ == '__main__':
         histogram = image.histogram()
         histograms.append(histogram)
 
-    # for histogram in histograms:
-    #     plt.figure()
-    #     # red histogram
-    #     for index in range(0, 256):
-    #         plt.bar(index, histogram[index], color = get_red_code(index), edgecolor = get_red_code(index), alpha = 0.3)
-    #     # green histogram
-    #     for index in range(256, 512):
-    #         plt.bar(index, histogram[index], color = get_green_code(index - 256), edgecolor = get_green_code(index - 256), alpha = 0.3)
-    #     # blue histogram
-    #     for index in range(512, 768):
-    #         plt.bar(index, histogram[index], color = get_blue_code(index - 512), edgecolor = get_blue_code(index - 512), alpha = 0.3)
-    #     print('Total length: ', np.sum(histogram))
-    # plt.show()
+    histograms = [histograms[0], histograms[5], histograms[10], histograms[12], histograms[17], histograms[22]]
 
-    print('Cosine similarity 1, 2: ', cosine_similarity([histograms[0]], [histograms[1]]))
-    print('Cosine similarity 1, 3: ', cosine_similarity([histograms[0]], [histograms[2]]))
-    print('Cosine similarity 1, 4: ', cosine_similarity([histograms[0]], [histograms[3]]))
-    print('Cosine similarity 2, 3: ', cosine_similarity([histograms[1]], [histograms[2]]))
-    print('Cosine similarity 2, 4: ', cosine_similarity([histograms[1]], [histograms[3]]))
-    print('Cosine similarity 3, 4: ', cosine_similarity([histograms[2]], [histograms[3]]))
+    for histogram in histograms:
+        plt.figure()
+        # red histogram
+        for index in range(0, 256):
+            plt.bar(index, histogram[index], color = get_red_code(index), edgecolor = get_red_code(index), alpha = 0.3)
+        # green histogram
+        for index in range(256, 512):
+            plt.bar(index, histogram[index], color = get_green_code(index - 256), edgecolor = get_green_code(index - 256), alpha = 0.3)
+        # blue histogram
+        for index in range(512, 768):
+            plt.bar(index, histogram[index], color = get_blue_code(index - 512), edgecolor = get_blue_code(index - 512), alpha = 0.3)
+        print('Total length: ', np.sum(histogram))
+    plt.show()
 
-    print('Euclidean distance 1, 2: ', euclidean_distances([histograms[0]], [histograms[1]]))
-    print('Euclidean distance 1, 3: ', euclidean_distances([histograms[0]], [histograms[2]]))
-    print('Euclidean distance 1, 4: ', euclidean_distances([histograms[0]], [histograms[3]]))
-    print('Euclidean distance 2, 3: ', euclidean_distances([histograms[1]], [histograms[2]]))
-    print('Euclidean distance 2, 4: ', euclidean_distances([histograms[1]], [histograms[3]]))
-    print('Euclidean distance 3, 4: ', euclidean_distances([histograms[2]], [histograms[3]]))
+    # print('Cosine similarity 1, 2: ', cosine_similarity([histograms[0]], [histograms[1]]))
+    # print('Cosine similarity 1, 3: ', cosine_similarity([histograms[0]], [histograms[2]]))
+    # print('Cosine similarity 1, 4: ', cosine_similarity([histograms[0]], [histograms[3]]))
+    # print('Cosine similarity 2, 3: ', cosine_similarity([histograms[1]], [histograms[2]]))
+    # print('Cosine similarity 2, 4: ', cosine_similarity([histograms[1]], [histograms[3]]))
+    # print('Cosine similarity 3, 4: ', cosine_similarity([histograms[2]], [histograms[3]]))
+
+    # print('Euclidean distance 1, 2: ', euclidean_distances([histograms[0]], [histograms[1]]))
+    # print('Euclidean distance 1, 3: ', euclidean_distances([histograms[0]], [histograms[2]]))
+    # print('Euclidean distance 1, 4: ', euclidean_distances([histograms[0]], [histograms[3]]))
+    # print('Euclidean distance 2, 3: ', euclidean_distances([histograms[1]], [histograms[2]]))
+    # print('Euclidean distance 2, 4: ', euclidean_distances([histograms[1]], [histograms[3]]))
+    # print('Euclidean distance 3, 4: ', euclidean_distances([histograms[2]], [histograms[3]]))
